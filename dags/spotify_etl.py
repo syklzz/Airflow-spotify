d0279c791400ef22ec8fdc6f19a6f7f9cc10b1b9
Elzbieta Wierciak <e.wierciak@gmail.com>
2023-12-14 22:37:32 +0100

    update + prezka

--- dags/spotify_etl.py
+++ dags/spotify_etl.py
@@ -6,8 +6,12 @@ MIDDLE = 1
 HIGH = 2
 BUCKET_COL = 'loudness'
 GROUP_COL = 'track_genre'
-DB_LOCATION = '/Users/aszynali/my_workspace/Airflow-spotify/spotify.db'
-DATA_PATH = '/Users/aszynali/my_workspace/Airflow-spotify/data/spotify_songs.csv'
+
+# DB_LOCATION = '/Users/aszynali/my_workspace/Airflow-spotify/spotify.db'
+# DATA_PATH = '/Users/aszynali/my_workspace/Airflow-spotify/data/spotify_songs.csv'
+
+DB_LOCATION = '/home/ewier/Desktop/adzd_project/spotify.db'
+DATA_PATH = '/home/ewier/Desktop/adzd_project/Airflow-spotify/data/dataset_2mb.csv'
 
 
 def save_data(df, name):
