60ce2ce91b2f78c5e584dd551be15157ee5dce15
Agnieszka Szynalik <aszynali@akamai.com>
2023-12-14 01:36:41 +0100

    Refactoring

--- dags/spotify_etl.py
+++ dags/spotify_etl.py
@@ -21,9 +21,9 @@ def calculate_loudness_ranges():
     return pd.DataFrame({'x0': [x0], 'x1': [x1], 'x2': [x2], 'x3': [x3]})
 
 
-def count_genre_within_loudness_range_etl(loudness_range):
+def count_genres_within_loudness_range_etl(loudness_range):
     loudness_ranges = pd.read_csv('../data/ranges.csv')
-    df = count_genre_within_loudness_range(loudness_range, loudness_ranges)
+    df = count_genres_within_loudness_range(loudness_range, loudness_ranges)
     if loudness_range == LOW:
         df.to_csv('../data/low_group.csv', index=False)
     elif loudness_range == MIDDLE:
@@ -32,7 +32,7 @@ def count_genre_within_loudness_range_etl(loudness_range):
         df.to_csv('../data/high_group.csv', index=False)
 
 
-def count_genre_within_loudness_range(loudness_range, loudness_ranges):
+def count_genres_within_loudness_range(loudness_range, loudness_ranges):
     df = pd.read_csv('../data/spotify_songs.csv')
     if loudness_range == LOW:
         min_threshold = loudness_ranges['x0'].values[0]
@@ -47,34 +47,22 @@ def count_genre_within_loudness_range(loudness_range, loudness_ranges):
     return df.groupby('playlist_genre').size().reset_index(name=f'count_{loudness_range}')
 
 
-def compare_loudness_range_for_genres_etl():
+def compare_genres_count_for_loudness_ranges_etl():
     low_df = pd.read_csv('../data/low_group.csv')
     middle_df = pd.read_csv('../data/middle_group.csv')
     high_df = pd.read_csv('../data/high_group.csv')
-    compare_loudness_range_for_genres(low_df, middle_df, high_df)
+    compare_genres_count_loudness_ranges(low_df, middle_df, high_df)
 
 
-def compare_loudness_range_for_genres(low_df, middle_df, high_df):
+def simple_compare_genres_count_for_loudness_ranges_etl():
+    loudness_ranges = calculate_loudness_ranges()
+    low_df = count_genres_within_loudness_range(LOW, loudness_ranges)
+    middle_df = count_genres_within_loudness_range(MIDDLE, loudness_ranges)
+    high_df = count_genres_within_loudness_range(HIGH, loudness_ranges)
+    compare_genres_count_loudness_ranges(low_df, middle_df, high_df)
+
+
+def compare_genres_count_loudness_ranges(low_df, middle_df, high_df):
     df = pd.merge(low_df, middle_df, on='playlist_genre', how='outer').fillna(0)
     df = pd.merge(df, high_df, on='playlist_genre', how='outer').fillna(0)
     df.to_csv('../data/merged.csv', index=False)
-
-
-if __name__ == "__main__":
-
-    # one dag flow test
-    ranges = calculate_loudness_ranges()
-    low_group = count_genre_within_loudness_range(LOW, ranges)
-    middle_group = count_genre_within_loudness_range(MIDDLE, ranges)
-    high_group = count_genre_within_loudness_range(HIGH, ranges)
-    compare_loudness_range_for_genres(low_group, middle_group, high_group)
-
-
-if __name__ == "__main__":
-
-    # multiple dag flow test
-    calculate_loudness_ranges_etl()
-    count_genre_within_loudness_range_etl(LOW)
-    count_genre_within_loudness_range_etl(MIDDLE)
-    count_genre_within_loudness_range_etl(HIGH)
-    compare_loudness_range_for_genres_etl()
