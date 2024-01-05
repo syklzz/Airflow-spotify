de71cf75ec2a234fed86daba390a29f733a58369
Agnieszka Szynalik <aszynali@akamai.com>
2024-01-05 14:13:06 +0100

    Add test dags

---
+++ dags/test_etl.py
@@ -0,0 +1,9 @@
+import time
+
+
+def run_start():
+    print('start')
+
+
+def run_sleep():
+    time.sleep(2)
---
+++ dags/test_long.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from test_etl import run_sleep, run_start
+from spotify_etl import run_simple_compare_result_for_bucket_ranges_etl, run_read_data_etl
+
+with DAG(
+        "test_long_dag",
+        default_args={
+            "depends_on_past": False,
+            "email": ["airflow@example.com"],
+            "email_on_failure": False,
+            "email_on_retry": False,
+            "retries": 1,
+            "retry_delay": timedelta(minutes=5),
+        },
+        description="A simple spotify DAG",
+        schedule=timedelta(days=1),
+        start_date=datetime(2021, 1, 1),
+        catchup=False,
+        tags=["simple_spotify", '2mb_dataset'],
+) as dag:
+    t0 = PythonOperator(
+        task_id='t0',
+        python_callable=run_start,
+        dag=dag,
+    )
+    t1 = PythonOperator(
+        task_id='t1',
+        python_callable=run_sleep,
+        dag=dag,
+    )
+    t2 = PythonOperator(
+        task_id='t2',
+        python_callable=run_sleep,
+        dag=dag,
+    )
+    t3 = PythonOperator(
+        task_id='t3',
+        python_callable=run_sleep,
+        dag=dag,
+    )
+    t4 = PythonOperator(
+        task_id='t4',
+        python_callable=run_sleep,
+        dag=dag,
+    )
+
+    t0 >> t1 >> t2 >> t3 >> t4
